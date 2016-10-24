48d75fa7d0213ad25e399c3a0ec79e64fb8348dc
ting <ct.shi@hnair.com>
2016-10-24 11:36:09 +0800

    wrap fabric transaction query rest api

---
+++ consonlesrvc/blockchain/bcconst.go
@@ -0,0 +1,20 @@
+package blockchain
+
+import (
+	"github.com/op/go-logging"
+	"baas/app-wallet/consonlesrvc/common"
+	"github.com/golang/protobuf/ptypes/timestamp"
+)
+
+var bcLogger *logging.Logger = common.NewLogger("blockchain")
+
+type BCTransactionMsg struct {
+	Type int `json:"type,omitempty"`
+	ChaincodeID string `json:"chaincodeID,omitempty"`
+	Payload string `json:"payload,omitempty"`
+	TxID string `json:"txid,omitempty"`
+	Timestamp timestamp.Timestamp `json:"timestamp,omitempty"`
+	Nonce string `json:"nonce,omitempty"`
+	Cert string `json:"cert,omitempty"`
+	Signature string `json:"signature,omitempty"`
+}
---
+++ consonlesrvc/blockchain/transaction.go
@@ -0,0 +1,103 @@
+package blockchain
+
+import (
+	"baas/app-wallet/consonlesrvc/auth"
+	"github.com/julienschmidt/httprouter"
+	"encoding/json"
+	"net/http"
+	"fmt"
+	"os"
+)
+
+type TransactionDetailRequest struct {
+	authsrvc.AuthRequest
+	BC_TXUUID string `json:"bc_txuuid"`
+}
+
+type TransactionDetailResponse struct {
+	authsrvc.AuthResponse
+	TxDetail *BCTransactionMsg `json:"txdetail,omitempty"`
+}
+
+type TransactionDetail struct {
+}
+
+
+func (t *TransactionDetail) post(req *TransactionDetailRequest)(*TransactionDetailResponse){
+	var res *TransactionDetailResponse = new(TransactionDetailResponse)
+	var err error
+
+	if !req.IsRequestValid(&res.AuthResponse) {
+		bcLogger.Warningf("request not valid: %#v", *req)
+		res.UserUUID = ""
+		return res
+	}
+
+	var peerAddr string
+	if peerAddr = os.Getenv("PEER_ADDRESS"); len(peerAddr) == 0 {
+		bcLogger.Fatal("failed getting environmental variable PEER_ADDRESS")
+		res.Status = "error"
+		res.Message = "failed getting peer address"
+		return res
+	}
+	peerAddr += "/transactions/" + req.BC_TXUUID
+
+	var peerResp *http.Response = new(http.Response)
+	if peerResp, err = http.Get(peerAddr); err != nil {
+		bcLogger.Errorf("failed querying blockchain transaction %s: %v",  req.BC_TXUUID, err)
+		res.Status = "error"
+		res.Message = "failed posint createAccount request to peer"
+		return res
+	}
+	defer peerResp.Body.Close()
+
+	if peerResp.StatusCode == http.StatusNotFound {
+		bcLogger.Errorf("transaction %s not exist in the blockchain", req.BC_TXUUID)
+		res.Status = "error"
+		res.Message = fmt.Sprintf("transaction %s not exist in the blockchain", req.BC_TXUUID)
+		return res
+	}
+
+	res.TxDetail = new(BCTransactionMsg)
+	if err = json.NewDecoder(peerResp.Body).Decode(res.TxDetail); err != nil {
+		bcLogger.Error("failed to decode transaction message from blockchain")
+		res.Status = "error"
+		res.Message = "failed to decode transaction message from blockchain"
+	}
+	bcLogger.Debugf("decoded transaction message : %#v", *res.TxDetail)
+	//var buf = new(bytes.Buffer)
+	//io.Copy(buf, peerResp.Body)
+	//res.TxDetail = buf.String()
+	//res.TxDetail = fmt.Sprintf("%s", buf.String())
+
+	bcLogger.Debugf("got blockchain transaction %s: %#v", req.BC_TXUUID, res.TxDetail)
+	return res
+}
+
+func TransactionDetailPost(w http.ResponseWriter, r *http.Request, _ httprouter.Params){
+	var err error
+	var req *TransactionDetailRequest = new(TransactionDetailRequest)
+	var res *TransactionDetailResponse
+	var resBytes []byte
+
+	w.Header().Set("Content-Type", "application/json")
+
+	if err = r.ParseForm(); err != nil {
+		bcLogger.Fatalf("failed to parse request for url %s: %v", r.URL.Path, err)
+	}
+
+	req.Username = r.PostForm.Get("username")
+	req.SessionID = r.PostForm.Get("sessionid")
+	req.AuthToken = r.PostForm.Get("authtoken")
+	req.BC_TXUUID = r.PostForm.Get("bc_txuuid")
+	bcLogger.Debugf("parsed request for url %s: %#v", r.URL.Path, req)
+
+	var t TransactionDetail
+	res = t.post(req)
+
+	resBytes, err = json.Marshal(*res)
+	if err != nil {
+		bcLogger.Fatalf("failed to marshal response as []byte: %v", err)
+	}
+	fmt.Fprintf(w, "%s", string(resBytes))
+}
